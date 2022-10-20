4f1de66e1cffad8e52457a6c1b283bc0de30e606
LostBlackCat <wxy1999814@gmail.com>
2022-10-20 21:35:15 +0800

    对cube进行类抽象 更新object基类

--- src/main.cpp
+++ src/main.cpp
@@ -122,105 +122,6 @@ int main() {
 	glfwSetInputMode(mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 
-	
-	// 发光cube所使用的顶点数组
-	// 只构造了所有的顶点 需要element_index_array来确定绘制顺序
-	float vertices[] = {
-		 0.0f,  0.0f,  0.0f,	//0
-		 1.0f,  0.0f,  0.0f,  	//1
-		 0.0f,  1.0f,  0.0f,	//2
-		 1.0f,  1.0f,  0.0f,	//3
-		 0.0f,  0.0f,  1.0f,	//4
-		 1.0f,  0.0f,  1.0f,	//5
-		 0.0f,  1.0f,  1.0f,	//6
-		 1.0f,  1.0f,  1.0f, 	//7
-	};
-
-
-	// textured cube所使用的顶点数组
-	// 包含了所有三角形的顶点 以及纹理坐标 以及每个绘制顶点的法向量
-	float vertices_array_mode[] = {
-		// ---position--		--texture--  --normal--
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,    0.0f,  0.0f, -1.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,    0.0f,  0.0f, -1.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,    0.0f,  0.0f, -1.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,    0.0f,  0.0f, -1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,    0.0f,  0.0f, -1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,    0.0f,  0.0f, -1.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,    0.0f,  0.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,     0.0f,  0.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,	 0.0f,  0.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,	 0.0f,  0.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,	 0.0f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,	 0.0f,  0.0f, 1.0f,
-
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,	-1.0f,  0.0f,  0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,	-1.0f,  0.0f,  0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,   -1.0f,  0.0f,  0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,	-1.0f,  0.0f,  0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,	-1.0f,  0.0f,  0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,	-1.0f,  0.0f,  0.0f,
-
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,	 1.0f,  0.0f,  0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,	 1.0f,  0.0f,  0.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,	 1.0f,  0.0f,  0.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,	 1.0f,  0.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,	 1.0f,  0.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,	 1.0f,  0.0f,  0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,	 0.0f, -1.0f,  0.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,	 0.0f, -1.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,	 0.0f, -1.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,	 0.0f, -1.0f,  0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,	 0.0f, -1.0f,  0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,	 0.0f, -1.0f,  0.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,	 0.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,	 0.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,	 0.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,	 0.0f,  1.0f,  0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,	 0.0f,  1.0f,  0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,	 0.0f,  1.0f,  0.0f
-	};
-
-
-	// 对于发光cube的顶点的三角形绘制顺序
-	unsigned int elementIndex[] = {	
-		0,1,2,
-		1,2,3,
-		4,5,6,
-		5,6,7,
-		0,1,4,
-		1,4,5,
-		2,3,6,
-		3,6,7,
-		1,3,5,
-		3,5,7,
-		0,2,4,
-		2,4,6
-	};
-
-
-	
-
-	// 初始化所需要的纹理图片
-	const int textureCount = 4;
-
-	Texture riceTexture(IMAGE_DIR"/rice.png", GL_RGBA);
-	Texture hutaoTexture(IMAGE_DIR"/hutao.png", GL_RGBA);
-	Texture jjzTexture(IMAGE_DIR"/jjz.jpg", GL_RGB);
-	Texture chenTexture(IMAGE_DIR"/chen.jpg", GL_RGB);
-
-	Texture textureList[textureCount] = { riceTexture, hutaoTexture, jjzTexture, chenTexture };
-
-	// 初始化镜面反射纹理图片
-	Texture riceSpec(IMAGE_DIR"/rice_spec.png", GL_RGBA);
-	Texture hutaoSpec(IMAGE_DIR"/hutao_spec.png", GL_RGBA);
-	Texture jjzSpec(IMAGE_DIR"/jjz_spec.jpg", GL_RGB);
-	Texture chenSpec(IMAGE_DIR"/chen_spec.jpg", GL_RGB);
-
-	Texture specList[textureCount] = { riceSpec, hutaoSpec, jjzSpec, chenSpec };
 
 
 	// 初始化天空盒所需的纹理图片
@@ -239,50 +140,28 @@ int main() {
 
 
 
-	// 创建VAO baseVAO中用来存放光照cube
-	unsigned int baseVAO;
-	glGenVertexArrays(1, &baseVAO);
-	glBindVertexArray(baseVAO);
+	// 注册cube
+    vector<CubeWithTex> cubeVector;
+    CubeWithTex cube_rice( IMAGE_DIR"/rice.png", IMAGE_DIR"/rice_spec.png");
+    CubeWithTex cube_chen( IMAGE_DIR"/chen.jpg", IMAGE_DIR"/chen_spec.png");
+    CubeWithTex cube_jjz( IMAGE_DIR"/jjz.jpg", IMAGE_DIR"/jjz_spec.jpg");
+    CubeWithTex cube_hutao(IMAGE_DIR"/hutao.png", IMAGE_DIR"/hutao_spec.png");
+    cubeVector.push_back(cube_rice);
+    cubeVector.push_back(cube_chen);
+    cubeVector.push_back(cube_jjz);
+    cubeVector.push_back(cube_hutao);
+    for(int i = 0;i<cubeVector.size();i++){
+        cubeVector[i].translation(glm::vec3(0.0f, 0.5f, (float)i * 2 + 0.0f));
+    }
 
-	unsigned int tri_vertex;
-	unsigned int tri_index;
 	glm::vec3 light_postion(2.0f, 2.0f, 0.0f);
-	glGenBuffers(1, &tri_vertex);
-	glBindBuffer(GL_ARRAY_BUFFER, tri_vertex);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glGenBuffers(1, &tri_index);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, tri_index);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elementIndex), elementIndex, GL_STATIC_DRAW);
-	//map the layout in vertex shader
-	//--------------index, size, type, ifNormalize, stride(offset), pointer(the space between first space)-------  
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0); //enable location = 0
-	glBindVertexArray(0);
+    glm::vec3 origin_position(0.0f, 0.5f, 0.0f);
 
-	// 创建第二个VAO cube_rice中存放了纹理cube
-	unsigned int cube_rice;
-	unsigned int rice_vertex;
-	glm::vec3 origin_position(0.0f, 0.5f, 0.0f); // 初始位置(世界坐标系中的位置)
-	glGenBuffers(1, &rice_vertex);
-	glGenVertexArrays(1, &cube_rice);
-	glBindVertexArray(cube_rice);
-	glBindBuffer(GL_ARRAY_BUFFER, rice_vertex);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_array_mode), vertices_array_mode, GL_STATIC_DRAW);
-	// 启用并绑定对应的顶点属性
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)nullptr);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(5 * sizeof(float)));
-	glEnableVertexAttribArray(0); //enable location = 0
-	glEnableVertexAttribArray(1); //enable location = 1
-	glEnableVertexAttribArray(2); //enable location = 2
-    glBindVertexArray(0);
+
 
 	Shader shader1(SHADER_DIR"/vertexShader.vs", SHADER_DIR"/fragmentShader.fs");
 	Shader shader_floor(SHADER_DIR"/floorVertexShader.vs", SHADER_DIR"/floorFragShader.fs");
     Shader shader_skyBox(SHADER_DIR"/skyVertexShader.vs", SHADER_DIR"/skyFragShader.fs");
-
-
-
 
 
 	float gree_axis_value = 1.0f;
@@ -328,7 +207,7 @@ int main() {
 		}
 
 		// 修正照相机在FPS模式下的位置 如果y值高于1.0, 就开始自由下落直到1.0
-		if (myCamera.position.y > 1.0f && myCamera.ifFpsMode == true){
+		if (myCamera.position.y > 1.0f && myCamera.ifFpsMode){
 			myCamera.position.y -= cameraSpeed;
 			if (myCamera.position.y < 1.0f)
 				myCamera.position.y = 1.0f;
@@ -377,29 +256,17 @@ int main() {
         myFloor.draw(shader1, projection, view);
 
         shader1.uniform_vec3(myCamera.position, "viewPos");
-        shader1.uniform_mat4(view, "viewMat");
-        shader1.uniform_mat4(projection, "projectionMat");
-		glBindVertexArray(cube_rice);
 
-		glm::vec3 current_postition(origin_position.x, origin_position.y, origin_position.z);
-		for (int i = 0; i < 10; i++) {
-			current_postition.z = 0.0f;
-			for (int j = 0; j < 10; j++) {
-				model = glm::translate(model, current_postition);
-				glActiveTexture(GL_TEXTURE2);
-				shader1.uniform_mat4(model, "modelMat");
-				glBindTexture(GL_TEXTURE_2D, textureList[(i * j) * 5 % 4].textureID);
-				glUniform1i(glGetUniformLocation(shader1.ID, "material.objectTexture"), 2);
-				glActiveTexture(GL_TEXTURE3); // 启用纹理1 作为镜面反射纹理
-				glBindTexture(GL_TEXTURE_2D, specList[(i * j) * 5 % 4].textureID);
-				glUniform1i(glGetUniformLocation(shader1.ID, "material.specTexture"), 3);
-				glDrawArrays(GL_TRIANGLES, 0, 256);
-				
-				current_postition.z += 2.0f;
-				model = glm::mat4(1.0f); // 重置model矩阵, 不能直接在之前的基础上做变换
-			}
-			current_postition.x += 2.0f;
-		}
+        glm::vec3 current_position(origin_position.x, origin_position.y, origin_position.z);
+        for (int i = 0; i < 10; i++) {
+            current_position.z = 0.0f;
+            for (int j = 0; j < 10; j++) {
+                cubeVector[i * j * 13 % 4].draw(shader1, projection, view);
+                current_position.z += 2.0f;
+                model = glm::mat4(1.0f); // 重置model矩阵, 不能直接在之前的基础上做变换
+            }
+            current_position.x += 2.0f;
+        }
 		
 		
 
